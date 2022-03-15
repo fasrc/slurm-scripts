288fce59df686642f5973d77c9d7e91d60f8c88e
root <root@fasse-slurm01.rc.fas.harvard.edu>
2022-03-15 09:57:22 -0400

    Adding fairshare script for fasse.

---
+++ fairshare/fairshare-fasse.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This script sets the fairshare scores for labs.  Based on the 03/15/22 census with adjusted scores.
+
+sacctmgr -i modify account set fairshare=100
+sacctmgr -i modify account name=cnl set fairshare=692
+sacctmgr -i modify account name=anderson_lab set fairshare=148
+
