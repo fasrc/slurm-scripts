2f47076fd47daf3c7a401285f568708e10b7ec53
root <root@holy-slurm02.rc.fas.harvard.edu>
2023-01-30 11:47:54 -0500

    Moving census file.

--- fairshare/fairshare-cannon.sh
+++ fairshare/fairshare-cannon.sh
@@ -2,7 +2,7 @@
 #This script sets the fairshare scores for labs by reading for a csv file.
 #The csv file should be formatted as labname,fairshare
 
-scores=$(cat ./cannon-fairshare-census-1-30-23.csv)
+scores=$(cat cannon/cannon-fairshare-census-1-30-23.csv)
 
 sacctmgr -i modify account set fairshare=120
 
