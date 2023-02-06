d0e91e151f3815bf69a2c308a7a11b68c160d8dd
root <root@holy-slurm02.rc.fas.harvard.edu>
2023-02-06 11:37:46 -0500

    Updating census.

--- general/suspend-node.sh
+++ general/suspend-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nodelist=$(/usr/local/bin/lsload | grep 8a25 | awk '{{print $1}}')
+nodelist=$(/usr/local/bin/lsload | grep 8a29 | awk '{{print $1}}')
 #joblist=$(/usr/bin/squeue | grep -i held | awk '{if ($5 =="PD"){print $1}}')
 #joblist=$(/usr/bin/squeue --qos=doshi-velez -u mamasood | awk '{if ($5 =="PD"){print $1}}')
 #joblist=$(sprio -u $1 | awk '{if ($2 =="azari"){print $1}}')
@@ -15,12 +15,15 @@ echo $nodelist
 for j in $nodelist
 do
 	echo $j
-        joblist=$(/usr/bin/squeue -w $j | awk '{if ($5 =="R"){print $1}}')
+        joblist=$(/usr/bin/squeue -w $j | awk '{if ($5 =="S"){print $1}}')
+
+#        scontrol update state=drain nodename=$j reason="IB work"
 
         echo $joblist
         for i in $joblist
         do
           echo $i
-          scontrol suspend $i
+          scontrol resume $i
         done
+        scontrol update state=resume nodename=$j
 done
