85839a35fcb7cabecd46e6385ca895a938eb6500
root <root@holy-slurm02.rc.fas.harvard.edu>
2023-05-16 09:56:49 -0400

    Updating fairshare.

--- general/suspend-node.sh
+++ general/suspend-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nodelist=$(/usr/local/bin/lsload | grep 8a29 | awk '{{print $1}}')
+nodelist=$(/usr/local/bin/lsload | grep 8a31 | awk '{{print $1}}')
 #joblist=$(/usr/bin/squeue | grep -i held | awk '{if ($5 =="PD"){print $1}}')
 #joblist=$(/usr/bin/squeue --qos=doshi-velez -u mamasood | awk '{if ($5 =="PD"){print $1}}')
 #joblist=$(sprio -u $1 | awk '{if ($2 =="azari"){print $1}}')
@@ -15,7 +15,7 @@ echo $nodelist
 for j in $nodelist
 do
 	echo $j
-        joblist=$(/usr/bin/squeue -w $j | awk '{if ($5 =="S"){print $1}}')
+        joblist=$(/usr/bin/squeue -w $j | awk '{if ($5 =="s"){print $1}}')
 
 #        scontrol update state=drain nodename=$j reason="IB work"
 
