4db64f58d4c49169fa9ac141718660469f88f0de
Paul Edmon <pedmon@DESKTOP-5GBIA4B.localdomain>
2023-06-27 10:38:11 -0400

    Adding fairshare-kempner script.

---
+++ fairshare/fairshare-kempner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This script sets up the Kempner fairshare groups. Note that this does not set the actual values that is done in the main Cannon fairshare script.
+# Instead this script creates the hierarchy for kempner as its different than our standard setup.
+
+kempner_pi_labs=$(alvarez_lab barak_lab bsabatini_lab jfrankle_lab konkle_lab pehlevan_lab)
+
+for lab in $kempner_pi_labs
+do
+  echo $lab
+  echo sacctmgr -i create account name=kempner_$lab fairshare=0
+done
