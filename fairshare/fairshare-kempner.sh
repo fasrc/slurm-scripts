fbf1178330eade2f4a55698d1fb0ca9487e7880d
root <root@holy-slurm02.rc.fas.harvard.edu>
2023-06-27 10:40:50 -0400

    Fixing.

--- fairshare/fairshare-kempner.sh
+++ fairshare/fairshare-kempner.sh
@@ -2,7 +2,7 @@
 # This script sets up the Kempner fairshare groups. Note that this does not set the actual values that is done in the main Cannon fairshare script.
 # Instead this script creates the hierarchy for kempner as its different than our standard setup.
 
-kempner_pi_labs=$(alvarez_lab barak_lab bsabatini_lab jfrankle_lab konkle_lab pehlevan_lab)
+kempner_pi_labs=$(echo alvarez_lab barak_lab bsabatini_lab jfrankle_lab konkle_lab pehlevan_lab)
 
 for lab in $kempner_pi_labs
 do
