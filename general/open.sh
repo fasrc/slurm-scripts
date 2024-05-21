384606640c6464ae292123b11a7cb518184a3d98
root <root@fasse-slurm01.rc.fas.harvard.edu>
2024-05-21 13:32:35 -0400

    Adding lsload to open.

--- general/open.sh
+++ general/open.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+nodelist=$(/usr/local/bin/lsload | grep respond | awk '{{print $1}}')
 #nodelist=$(sinfo -p fas_gpu -o "%60E %12U %19H %6t %n" | sed -e '/none/d' | sort |uniq | awk '{print $7}')
 #nodelist=$(sinfo -o "%n %60E %12U %19H %6t "  | sed -e '/none/d' | sort |uniq | grep 'Kill task failed'| awk '{ print $1 }')
 #nodelist=$(sinfo -o "%n %60E %12U %19H %6t "  | sed -e '/none/d' | sort |uniq | grep 'procs still running'| awk '{ print $1 }')
 #nodelist=$(/usr/local/bin/lsload | grep DOWN | grep env | awk '{{print $1}}')
-nodelist=$(sinfo -p sapphire --long --list-reasons |  grep responding | awk '{{print $6}}')
+#nodelist=$(sinfo -p sapphire --long --list-reasons |  grep responding | awk '{{print $6}}')
 #nodelist=$(sinfo -p kempner --long --list-reasons | grep Kill | awk '{{print $7}}')
 #joblist=$(/usr/bin/squeue | grep -i held | awk '{if ($5 =="PD"){print $1}}')
 #joblist=$(/usr/bin/squeue --qos=doshi-velez -u mamasood | awk '{if ($5 =="PD"){print $1}}')
@@ -18,6 +19,7 @@ echo $nodelist
 
 for j in $nodelist
 do
+        j=${j//\(d\)}
 	echo $j
 	scontrol update state=resume nodename=$j
 done
